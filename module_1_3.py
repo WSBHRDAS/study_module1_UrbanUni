85d04e77405d785c7c47719d436cb591f11f7559
WSBHRDAS <chelnea@gmail.com>
2024-11-20 21:19:01 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name="WSBHRDAS"
+print ('Name : ' + name)
+
+age = 39
+print ('Age : ' + str(age))
+age = age + 1
+print ('New Age : ' + str(age))
+
+is_student = True
+print ('Is Student : ' + str(is_student))
+
+name = ['1',2]
+print (name[1]+3, str(type(name)))
