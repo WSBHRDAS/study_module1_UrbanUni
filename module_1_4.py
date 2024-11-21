9ecd524a6499e78e8ab12e28223c349683031975
WSBHRDAS <chelnea@gmail.com>
2024-11-21 21:38:01 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+#6th program
+
+first_name = "Vasya"
+
+apple_count = 10
+
+second_name = "Petya"
+
+orange_count = 15
+
+print (first_name,"дал",second_name, apple_count,"яблок"+",а также",orange_count, "апельсинов")
