b220ec081ed8b1a3c0bd07a7815b21cd66752862
WSBHRDAS <chelnea@gmail.com>
2024-11-22 23:15:45 +0300

    version 2
    v.2

--- module_1_5.py
+++ module_1_5.py
@@ -1,13 +1,29 @@
-#7th program
+immutable_var = tuple([True,["Arsen","Vardanyan"],"man",32,"Georgia"])
 
-example = 'Апельсин'
+print("Immutable tuple:",immutable_var)
 
-print (example[0])
+#immutable_var[1]=12.1 #ERROR! Other type of element Нельзя менять значения элементов 
+# кортежа, однако содержание списков - элементов кортежа можно менять,обращаясь к элементам
+# такого списка
 
-print (example[-1])
+immutable_var[1][0] = "Sergey"#,"Anokhin"]
 
-print (example[4:])
+immutable_var[1][1] = 1#"Anokhin"
 
-print (example[::-1])
+print("Immutable tuple:",immutable_var)
 
-print (example[1::2])
+mutable_list = list([True,["Arsen","Vardanyan"],"man",32,"Georgia"])
+
+print (mutable_list)
+
+mutable_list[1]=["Pacим","Абушариф","Оглы"]
+
+mutable_list.append("12.01.1983")
+
+mutable_list.extend("A3")
+
+print (mutable_list)
+
+mutable_list.remove ("man")
+
+print (mutable_list)
